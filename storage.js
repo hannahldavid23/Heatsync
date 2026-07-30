21e790b8ef4cc6110271b0b0d285377d7beb6ffd
hannahldavid23 <mvvvsybvgb@privaterelay.appleid.com>
2026-07-30 11:58:00 -0500

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,63 @@
+const savedShiftKey = "HeatSyncActiveShift";
 
+
+function saveShiftData(shiftData, rotationTime, timersPaused){
+
+let data = {
+
+rotationTime: rotationTime,
+
+shiftData: shiftData,
+
+timersPaused: timersPaused,
+
+savedAt: Date.now()
+
+};
+
+
+localStorage.setItem(
+savedShiftKey,
+JSON.stringify(data)
+);
+
+}
+
+
+
+function loadShiftData(){
+
+let saved =
+localStorage.getItem(savedShiftKey);
+
+
+if(!saved){
+
+return null;
+
+}
+
+
+try{
+
+return JSON.parse(saved);
+
+}
+
+catch(error){
+
+console.log("Could not load saved shift:", error);
+
+return null;
+
+}
+
+}
+
+
+
+function clearShiftData(){
+
+localStorage.removeItem(savedShiftKey);
+
+}
